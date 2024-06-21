b7541aba70ee9235e1849a428b229f71736e5b76
Herman Zhao <hermanzhaozzzz@gmail.com>
2024-06-22 01:05:51 +0800

    decoupling zshrc and zshenv

---
+++ spyder/general/spyder-py3/template.py
@@ -0,0 +1,23 @@
+"""Spyder Project: This is a temporary script file.
+
+Created on XXXX-XX-XX
+
+@author: Herman ZHAO
+"""
+
+# %% library
+import os
+
+import numpy as np
+import pandas as pd
+# %%% pandas settings
+# pd.set_option("max_colwidth", 200)  # column最大宽度
+pd.set_option("display.width", 120)  # dataframe宽度
+pd.set_option("display.max_columns", None)  # column最大显示数
+pd.set_option("display.max_rows", 50)  # row最大显示数
+# %% set directory
+os.chdir("./")
+os.listdir()
+
+# %% start
+

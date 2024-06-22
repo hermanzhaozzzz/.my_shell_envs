087b051d14bd9f64384f13b0488a9676783b73d1
Herman Zhao <hermanzhaozzzz@gmail.com>
2024-06-22 14:23:41 +0800

    ri

--- apply_personal_envs.sh
+++ apply_personal_envs.sh
@@ -1,4 +1,4 @@
 REPO_PATH=$(pwd)
-/bin/rm -i $HOME/.zshenv_bak 2>/dev/null
+/bin/rm -f $HOME/.zshenv_bak 2>/dev/null
 mv $HOME/.zshenv $HOME/.zshenv_bak
 ln -s $REPO_PATH/zsh/zshenv_demo $HOME/.zshenv

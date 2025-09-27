fa2ea5e628fb425fa57802bf4acb9c4242c5c49b
Huanan Zhao (赵华男) <39041821+hermanzhaozzzz@users.noreply.github.com>
2025-09-28 00:37:02 +0800

    Update deploying_locally.sh

--- deploying_locally.sh
+++ deploying_locally.sh
@@ -241,8 +241,8 @@ echo "set tldr successful"
 if [[ "$platform" == "Windows" ]]; then
 	cd $REPO_PATH
 
-    mkdir -p "/c/Program Files/PowerShell/7"
-	WinProfile="/c/Program Files/PowerShell/7/profile.ps1"
+    mkdir -p "/c/Program\ Files/PowerShell/7"
+	WinProfile="/c/Program\ Files/PowerShell/7/profile.ps1"
 	RepoProfile="$REPO_PATH/powershell/Microsoft.PowerShell_profile.ps1"
 	echo -e "---------------------------------|\nset profile softlink @ $WinProfile..."
 	/bin/rm -rf $HOME/.condarc 2>/dev/null
